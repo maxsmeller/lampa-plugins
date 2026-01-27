8443ce39e28fb9c761f389751c924efe613f10e2
maxsmeller <143477158+maxsmeller@users.noreply.github.com>
2026-01-27 11:30:34 +0700

    Update lich.js

--- lich.js
+++ lich.js
@@ -1,18 +1,19 @@
 (function() {
     'use strict';
 
-    // Основная функция применения стилей
-    function applyStyle() {
+    // Основная функция применения стилей и добавления шаблонов
+    function applyStyleAndTemplates() {
         // Проверяем, не добавлены ли уже стили
         if (document.getElementById('maxsm_lich_style')) {
             return;
         }
         
-        // Удаляем старые стили
+        // Удаляем старые стили если jQuery доступен
         if (typeof window.$ !== 'undefined' && window.$ && window.$.fn) {
             $('#maxsm_lich_style').remove();
         }
         
+        // Создаем элемент стилей
         var style = document.createElement('style');
         style.id = 'maxsm_lich_style';
         
@@ -48,7 +49,6 @@
                 'position: absolute !important;' +
                 'right: 0em !important;' +
                 'bottom: 0em !important;' +
-           //     'z-index: 10 !important;' +
                 'font-weight: 700 !important;' +
                 'padding: 0.4em 0.8em !important;' +
                 'border-radius: 1em 0 1em 0 !important;' +
@@ -80,8 +80,7 @@
                 'padding: 0.4em 0.8em !important;' +
                 'font-weight: 700 !important;' +
                 'font-size: 1.0em !important;' +
-                'border-radius: 1em 0 1em 0 !important;' + 
-            //    'text-transform: uppercase !important;' +
+                'border-radius: 1em 0 1em 0 !important;' +
             '}' +
             
             // Тип сериал
@@ -96,7 +95,6 @@
                 'border-radius: 1em 0 1em 0 !important;' +
                 'line-height: 1.0 !important;' +
                 'font-size: 1.0em !important;' +
-             //   'z-index: 5 !important;' +
             '}' +
             
             // Расстояние между рядами
@@ -129,78 +127,71 @@
         
         document.head.appendChild(style);
         
-        // Добавление шаблонов
-        addTemplates();
-    }
-    
-    // Функция добавления шаблонов
-    function addTemplates() {
-        // Проверяем существование объекта Lampa
-        if (typeof Lampa === 'undefined' || !Lampa.Template) {
-            console.warn('Lampa.Template не найден');
-            return;
-        }
-        
-        // Шаблон карточки с годом выше названия
-        Lampa.Template.add('card', 
-            '<div class="card selector layer--visible layer--render">' +
-                '<div class="card__view">' +
-                    '<img src="../img/img_load.svg" class="card__img" />' +
-                    '<div class="card__icons">' +
-                        '<div class="card__icons-inner"></div>' +
-                    '</div>' +
-                    '<div class="card__age">{release_year}</div>' +
-                '</div>' +
-                '<div class="card__title">{title}</div>' +
-            '</div>');
-        
-        /* Шаблон карточки эпизода без футера
-        Lampa.Template.add('card_episode',
-            '<div class="card-episode selector layer--visible layer--render">' +
-                '<div class="card-episode__body">' +
-                    '<div class="full-episode">' +
-                        '<div class="full-episode__img">' +
-                            '<img />' +
+        // Добавление шаблонов если доступен Lampa.Template
+        if (typeof Lampa !== 'undefined' && Lampa.Template) {
+            // Шаблон карточки с годом выше названия
+            Lampa.Template.add('card', 
+                '<div class="card selector layer--visible layer--render">' +
+                    '<div class="card__view">' +
+                        '<img src="../img/img_load.svg" class="card__img" />' +
+                        '<div class="card__icons">' +
+                            '<div class="card__icons-inner"></div>' +
                         '</div>' +
-                        '<div class="full-episode__body">' +
-                            '<div class="card__title">{title}</div>' +
-                            '<div class="card__age">{release_year}</div>' +
-                            '<div class="full-episode__num hide">{num}</div>' +
-                            '<div class="full-episode__name">{name}</div>' +
-                            '<div class="full-episode__date">{date}</div>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>' +
-                '<div class="card-episode__footer hide">' +
-                    '<div class="card__imgbox">' +
-                        '<div class="card__view">' +
-                            '<img class="card__img" />' +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="card__left">' +
-                        '<div class="card__title">{title}</div>' +
                         '<div class="card__age">{release_year}</div>' +
                     '</div>' +
-                '</div>' +
-            '</div>'); */
+                    '<div class="card__title">{title}</div>' +
+                '</div>');
+            
+            // Шаблон карточки эпизода без футера
+            Lampa.Template.add('card_episode',
+                '<div class="card-episode selector layer--visible layer--render">' +
+                    '<div class="card-episode__body">' +
+                        '<div class="full-episode">' +
+                            '<div class="full-episode__img">' +
+                                '<img />' +
+                            '</div>' +
+                            '<div class="full-episode__body">' +
+                                '<div class="card__title">{title}</div>' +
+                                '<div class="card__age">{release_year}</div>' +
+                                '<div class="full-episode__num hide">{num}</div>' +
+                                '<div class="full-episode__name">{name}</div>' +
+                                '<div class="full-episode__date">{date}</div>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="card-episode__footer hide">' +
+                        '<div class="card__imgbox">' +
+                            '<div class="card__view">' +
+                                '<img class="card__img" />' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="card__left">' +
+                            '<div class="card__title">{title}</div>' +
+                            '<div class="card__age">{release_year}</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>');
+        } else {
+            console.warn('Lampa.Template не найден, шаблоны не добавлены');
+        }
     }
 
     // Запуск при загрузке приложения
     function init() {
         if (window.appready) {
-            applyStyle();
+            applyStyleAndTemplates();
         } else if (typeof Lampa !== 'undefined' && Lampa.Listener) {
             Lampa.Listener.follow('app', function(event) {
                 if (event.type === 'ready') {
-                    applyStyle();
+                    applyStyleAndTemplates();
                 }
             });
         } else {
             // Fallback: запускаем при полной загрузке DOM
             if (document.readyState === 'loading') {
-                document.addEventListener('DOMContentLoaded', applyStyle);
+                document.addEventListener('DOMContentLoaded', applyStyleAndTemplates);
             } else {
-                applyStyle();
+                applyStyleAndTemplates();
             }
         }
     }
@@ -208,6 +199,3 @@
     // Запускаем инициализацию
     init();
 })();
-
-
-
